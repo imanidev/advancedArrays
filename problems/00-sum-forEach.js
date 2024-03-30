1a9d4b83e3791bc119853f36b478159cbb1e9910
Imani Roberts <imaniroberts.dev@gmail.com>
2024-03-30 14:44:22 -0400

    solved problem

--- problems/00-sum-forEach.js
+++ problems/00-sum-forEach.js
@@ -5,16 +5,20 @@ The function should return the total sum of all values in the array.
 Solve this using Array's `forEach()` method.
 
 Examples:
-
-console.log(sumForEach([4, 3, -1, 10])); // 16
-console.log(sumForEach([6, 7, 2])); // 15
 console.log(sumForEach([])); // 0
+console.log(sumForEach([4, 3, -1, 10])); // 16
 */
 
 let sumForEach = function(arr) {
-  // Your code here 
+  let sum = 0;
+  arr.forEach((num) => {
+    sum += num;
+  });
+  return sum;
 };
 
+
+console.log(sumForEach([6, 7, 2])); // 15
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
