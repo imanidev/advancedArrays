fd1ac9d7f511d5ec68ba07c67bb71e653cf0bc03
Imani Roberts <imaniroberts.dev@gmail.com>
2024-03-30 18:00:39 -0400

    problem 5 solved

--- problems/05-choosey-endings-filter.js
+++ problems/05-choosey-endings-filter.js
@@ -2,15 +2,12 @@
 Write a function `chooseyEndings` that accepts an array of words and a suffix
 string as arguments. The function should return a new array containing the words
 that end in the given suffix. If the value passed in is not an array, return an
-empty array. Solve this using Array's `filter()` method. 
+empty array. Solve this using Array's `filter()` method.
 
 HINT: There are built in JavaScript functions that will help with determining if
 a strings ends a certain way. Go see if you can find it on MDN!
 
 Examples:
-
-console.log(chooseyEndings(['family', 'hound', 'catalyst', 'fly', 'timidly', 'bond'], 'ly'));
-// [ 'family', 'fly', 'timidly' ]
 
 console.log(chooseyEndings(['family', 'hound', 'catalyst', 'fly', 'timidly', 'bond'], 'nd'));
 // [ 'hound', 'bond' ]
@@ -26,8 +23,17 @@ console.log(chooseyEndings(17, 'ily'));
 */
 
 let chooseyEndings = function(words, suffix) {
-  // Your code here 
+  let newWords = [];
+  words.filter((word) => {
+    if (word.endsWith(suffix)) {
+      newWords.push(word)
+    }
+  });
+  return newWords;
 };
+
+console.log(chooseyEndings(['family', 'hound', 'catalyst', 'fly', 'timidly', 'bond'], 'ly'));
+// [ 'family', 'fly', 'timidly' ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
